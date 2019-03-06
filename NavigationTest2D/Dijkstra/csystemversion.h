38c57294c286a0907fcd79dad7436ba9ddaab217
Manaf_Mehrabi <M.Mehrabi@faradid-co.com>
2019-03-06 09:41:37 +0330

    Version1.2.0 : OpenGL is implementing.

--- NavigationTest2D/Dijkstra/csystemversion.h
+++ NavigationTest2D/Dijkstra/csystemversion.h
@@ -1,10 +1,10 @@
 #ifndef CSYSTEMVERSION_H
 #define CSYSTEMVERSION_H
 
-//97-12-15      Some bugs have been fixed.
+//97-12-15      OpenGL is implementing.
 
 #define MAJOR_VERSION 1;
-#define MINOR_VERSION 1;
-#define BUILD_VERSION 4;
+#define MINOR_VERSION 2;
+#define BUILD_VERSION 0;
 
 #endif
--- NavigationTest2D/main.cpp
+++ NavigationTest2D/main.cpp
@@ -4,46 +4,53 @@
 #include "ShapeFile/cshapefile.h"
 #include "Dijkstra/cdijkstra.h"
 
+
+#endif
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
+
+
+
     //std::string path = "E:/Workstation/C++/NavigationTest2D/Data/test/test.shp";
     //CShapeFile shapeFile;
     //shapeFile.OpenFile(path);
 
-    CCoordinate a(0, 0, 0), b(5, 1, 0), c(7, 0, 0), d(3, 2, 0), e(4, 5, 0), f(9, 5, 0);
-    CDijkstra dijkstra;
-    dijkstra.AddWay(CWay(a, b, true));
-    dijkstra.AddWay(CWay(a, d, false));
-    dijkstra.AddWay(CWay(b, c, true));
-    dijkstra.AddWay(CWay(d, e, false));
-    dijkstra.AddWay(CWay(e, c, true));
-    dijkstra.AddWay(CWay(d, f, false));
-    dijkstra.AddWay(CWay(f, c, true));
-    dijkstra.AddWay(CWay(e, b, true));
-    dijkstra.GenerateDistancesMatrix(6);
 
-    double pathDistance = 0;
-    std::vector<CCoordinate> shortestPath =
-        //dijkstra.FindShortestPath(a, c, pathDistance);//Succeeded
-        //dijkstra.FindShortestPath(d, c, pathDistance);//Succeeded
-        //dijkstra.FindShortestPath(c, f, pathDistance);//Succeeded
-        //dijkstra.FindShortestPath(e, a, pathDistance);//Succeeded
-        //dijkstra.FindShortestPath(e, f, pathDistance);//Succeeded
-        dijkstra.FindShortestPath(e, b, pathDistance);//Succeeded
-    std::cout << "\nShortest path has " << pathDistance << " distance and " << shortestPath.size() << " points:\n";
-    for (std::vector<CCoordinate>::iterator coordinates = shortestPath.begin();
-         coordinates != shortestPath.end();
-         coordinates++){
-        std::cout << "(" <<
-            coordinates->m_x << ", " <<
-            coordinates->m_y << ", " <<
-            coordinates->m_z << ")\n";
-    }
-    std::cout.flush();
+
+//    CCoordinate a(0, 0, 0), b(5, 1, 0), c(7, 0, 0), d(3, 2, 0), e(4, 5, 0), f(9, 5, 0);
+//    CDijkstra dijkstra;
+//    dijkstra.AddWay(CWay(a, b, true));
+//    dijkstra.AddWay(CWay(a, d, false));
+//    dijkstra.AddWay(CWay(b, c, true));
+//    dijkstra.AddWay(CWay(d, e, false));
+//    dijkstra.AddWay(CWay(e, c, true));
+//    dijkstra.AddWay(CWay(d, f, false));
+//    dijkstra.AddWay(CWay(f, c, true));
+//    dijkstra.AddWay(CWay(e, b, true));
+//    dijkstra.GenerateDistancesMatrix(6);
+
+//    double pathDistance = 0;
+//    std::vector<CCoordinate> shortestPath =
+//        //dijkstra.FindShortestPath(a, c, pathDistance);//Succeeded
+//        //dijkstra.FindShortestPath(d, c, pathDistance);//Succeeded
+//        //dijkstra.FindShortestPath(c, f, pathDistance);//Succeeded
+//        //dijkstra.FindShortestPath(e, a, pathDistance);//Succeeded
+//        //dijkstra.FindShortestPath(e, f, pathDistance);//Succeeded
+//        dijkstra.FindShortestPath(e, b, pathDistance);//Succeeded
+//    std::cout << "\nShortest path has " << pathDistance << " distance and " << shortestPath.size() << " points:\n";
+//    for (std::vector<CCoordinate>::iterator coordinates = shortestPath.begin();
+//         coordinates != shortestPath.end();
+//         coordinates++){
+//        std::cout << "(" <<
+//            coordinates->m_x << ", " <<
+//            coordinates->m_y << ", " <<
+//            coordinates->m_z << ")\n";
+//    }
+//    std::cout.flush();
 
     QApplication application(argc, argv);
     MainWindow window;
